82741cca8fbf2903511168dd1a90fbd9e5e7291e
Thomas Musselman <tmusselman@gmail.com>
2023-04-29 23:23:37 -0600

    move completed items to bottom of list

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,7 +6,7 @@ const TodoList = ({todos, deleteItem, toggleItem}) => {
       <h1 className='header'>Todo List</h1>
       <ul className='list'>
         {todos.length === 0 && "No Todos"}
-        {todos.map(todo => {
+        {todos.sort((a, b) => a.completed &&  !b.completed ? 1 : -1).map(todo => {
           return (
             <TodoItem {...todo} key={todo.id} onDelete={deleteItem} onToggle={toggleItem}/>
           )
